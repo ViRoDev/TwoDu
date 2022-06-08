6e25b42dc69e2e7d6917624e8948ad251f69e8b1
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-06-08 20:21:51 +0300

    FIXED silly port issue

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,11 @@
 import {Pool} from 'pg';
 
-const pool: Pool = new Pool(
+export const pool: Pool = new Pool(
     {
         user: "postgres",
-        password: "",
+        password: '',
         host: "localhost",
-        port: 3001,
+        port: 5432,
         database: "todo"
     }
 )
