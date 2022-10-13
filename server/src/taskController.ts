b4f0e1d239f62b39e097fdc9522f4eb5ef84b401
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-10-13 13:00:22 +0300

    move to Prisma ORM

--- server/src/taskController.ts
+++ server/src/taskController.ts
@@ -1,46 +1,49 @@
 import {Request, Response} from 'express'
-import { pool } from './db';
+import {PrismaClient} from '@prisma/client'
 
 class taskController {
+    private prisma : PrismaClient;
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
     //TODO: Timestamps, isDone, etc.
     async createTask(req: Request, res: Response) {
         const {title, comment, list_id} = req.body;
-        pool.query("INSERT INTO task (title, comment, list_id, is_done, created) \
-                    VALUES ($1, $2, $3, false, current_timestamp) \
-                    RETURNING *;", 
-                    [title, comment, list_id], 
-                    (error, results) => {
-                        if(error) throw error
-                        res = appendHeaders(res);
-                        //[0] for returning a single object, not an array
-                        res.json(results.rows[0])
-                    });
+        res.json( 
+            await this.prisma.task.create({
+                data: {
+                    title: title,
+                    comment: comment,
+                    list_id: list_id
+                },
+            })
+        )
     }
 
     async getAllTasks(req: Request, res: Response) {
-        console.log(req.query);
-        pool.query("SELECT * \
-                    FROM task \
-                    WHERE task.list_id = $1", 
-                    [req.query.list_id],
-                    (error, results) => {
-                        if(error) throw error
-                        res = appendHeaders(res);
-                        res.json(results.rows)
-                    });
+        const {list_id} = req.body;
+        res.json( 
+            await this.prisma.task.findMany({
+                where: {
+                    list_id: list_id,
+                },
+            })
+        )
     }
 
     async setDone(req: Request, res: Response) {
-        const task_id : number = req.body
-        pool.query("UPDATE task \
-                    SET is_done = true, done = current_timestamp \
-                    WHERE id = $1",
-                    [task_id],
-                    (error, results) => {
-                        if(error) throw error
-                        res = appendHeaders(res);
-                        res.json(results.rows)
-                    });
+        const {task_id} = req.body;
+        res.json(
+            await this.prisma.task.update({
+                where: {
+                    id: task_id,
+                },
+                data: {
+                    is_done: true,
+                }
+            })
+        )
     }
 }
 
