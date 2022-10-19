6af76b0723856c0026e098f29af3dc5e2822df77
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-10-20 01:03:26 +0300

    move test logic into seperate file

---
+++ server/src/jwt/jwt.test.ts
@@ -0,0 +1,27 @@
+import { Err, Ok, Result } from '../Result'
+import { SignTokenError } from './createToken'
+import {create, verify} from './jwt'
+
+const header : JWTHeader = {
+    alg: "HS256",
+    typ: "JWT"
+}
+
+const payload : Payload = {
+    sub: 1
+}
+
+const test = () : Result<boolean, SignTokenError> => {
+    const tk = create(header, payload)
+    if(!tk.ok) return Err(tk.error);
+    console.log(`Token: ${tk.data}`);
+
+    const verified = verify(tk.data);
+    if (!verified.ok) return Err(verified.error);
+
+    console.log(`Verify: ${verified.data}`);
+
+    return Ok(true);
+}
+test();
+
