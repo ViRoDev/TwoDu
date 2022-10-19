dd72e02e4aff91d707aa6d038196d85a6df167b4
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-10-19 15:10:37 +0300

    make token verification, read logic. Seperate sign logic. Add types

--- server/src/jwt.ts
+++ server/src/jwt.ts
@@ -14,23 +14,55 @@ type Payload = {
     sub: number
 }
 
-const createToken = () => {
-    const header : JWTHeader = {
-        alg: "HS256",
-        typ: "JWT"
-    }
-    const payload : Payload = {
-        sub: 1
-    }
-
+const createToken = (header : JWTHeader, payload : Payload) : Token => {
     const toSign = [header, payload]
         .map(obj => JSON.stringify(obj))
-        .map(json => base64url(json))
-        .join('.')
+        .map(json => base64url.encode(json))
+        .join('.') as HeaderPayload;
     
-    const signature = hs256(toSign, SECRET).toString()
+    const signature = signToken(toSign, SECRET);
 
-    return `${toSign}.${signature}`
+    return `${toSign}.${signature}` as Token;
 }
 
-console.log(createToken())
+type HeaderPayload = `${string}.${string}`
+const signToken = (hp : HeaderPayload, secret: string) => {
+    const header64 : string = hp.split(".").at(0) || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" // { "alg" : "HS256", "typ": "JWT"}
+    const header : JWTHeader = JSON.parse(base64url.decode(header64));
+    const alg : Alg = header.alg;
+    
+    //implement more algorythms?
+    switch (alg) {
+        case "HS256":
+            return hs256(hp, secret).toString()
+            break;
+    }
+}
+
+type Token = `${string}.${string}.${string}`
+const readToken = (tk : Token) => {
+    const [header64, payload64, signature] = tk.split('.');
+    const [header, payload] = [header64, payload64]
+        .map(b64url => base64url.decode(b64url))
+        .map(json => JSON.parse(json)) 
+
+    return [header, payload, signature] as [JWTHeader, Payload, string]
+}
+
+const verifyToken = (tk : Token) => {
+    const [header64, payload64, signature] = tk.split('.');
+    return signToken(`${header64}.${payload64}`, SECRET) === signature;
+}
+
+const header : JWTHeader = {
+    alg: "HS256",
+    typ: "JWT"
+}
+
+const payload : Payload = {
+    sub: 1
+}
+
+const tk = createToken(header, payload)
+console.log(`Token: ${tk}`);
+console.log(`Verify: ${verifyToken(tk)}`);
