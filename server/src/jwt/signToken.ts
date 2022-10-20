d41c6d4ec2b75ecef4be62bd7717c448e4ede581
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-10-20 19:04:29 +0300

    refactor to function chainging + add additional type checks

--- server/src/jwt/signToken.ts
+++ server/src/jwt/signToken.ts
@@ -1,6 +1,6 @@
 import base64url from "base64url";
 import { HmacSHA256 } from "crypto-js";
-import { Err, Ok, Result } from "../Result";
+import { Err, Ok, Result, wrap } from "../Result";
 import { Alg, HeaderBase64Url, JWTHeader, PayloadBase64Url } from "./jwt.types";
 
 export enum SignTokenError {
@@ -12,27 +12,23 @@ export enum SignTokenError {
 }
 
 export const signToken = (header : HeaderBase64Url, payload: PayloadBase64Url, secret: string) : Result<string,SignTokenError> =>  {
-    if(header === "") return Err(SignTokenError.HeaderEmptyError);
+    if(typeof header === undefined || header === "") return Err(SignTokenError.HeaderEmptyError);
+
+    const jwtHeader = [header]
+        .map(decodeBase64url)
+        .map(wrap(parseJson))
+        .at(0)
     
-    const decode = decodeBase64url(header);
-    if (!decode.ok) return Err(decode.error);
-    const json = decode.data;
+    if(typeof jwtHeader === 'undefined') return Err(SignTokenError.HeaderNotJsonError);
+    if(!jwtHeader.ok) return Err(jwtHeader.error) 
 
-    const parse = parseJson(json);
-    if (!parse.ok) return Err(parse.error);
-    const obj = parse.data;
-
-    if(obj.alg === undefined) return Err(SignTokenError.HeaderNoHashAlgorythmError);
-    
-    const algorythm = obj.alg as Alg;
-
+    const algorythm = jwtHeader.data.alg
     switch (algorythm) {
         case 'HS256':
-            return Ok(HmacSHA256(`${header}.${payload}`, secret).toString());
-            break;
+            const signature = HmacSHA256(`${header}.${payload}`, secret).toString();
+            return Ok(signature)
         default:
             return Err(SignTokenError.HeaderWrongHashAlgorythmError);
-            break;
     }
 }
 
@@ -45,9 +41,26 @@ const decodeBase64url = (str : string) : Result<string, SignTokenError> => {
     } catch { return Err(SignTokenError.UncaughtError); }
 }
 
+const checkAlgorythm = (obj : {alg: string}) : obj is JWTHeader => {
+    switch(obj.alg) {
+        case "HS256":
+            return true;
+        default:
+            return false;
+    }
+}
+
+const objectIsHeader = (obj : any) : obj is {alg : string} => 
+obj !== undefined 
+&& (obj.alg) !== undefined 
+&& typeof(obj.alg) === "string";
+
 const parseJson = (json : string) : Result<JWTHeader, SignTokenError> => {
     try {
         const obj = JSON.parse(json);
+        if(!objectIsHeader(obj)) return Err(SignTokenError.HeaderNotJsonError);
+        if(!checkAlgorythm(obj)) return Err(SignTokenError.HeaderWrongHashAlgorythmError);
+
         return Ok(obj)
-    } catch { return Err(SignTokenError.HeaderNotJsonError); }
+    } catch { return Err(SignTokenError.UncaughtError); }
 }
