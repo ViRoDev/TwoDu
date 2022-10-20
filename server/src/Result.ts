20245aa88c9dd161c80bc051aba4f92a7929080b
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-10-20 19:05:28 +0300

    fix 'any' type in Result to generics

--- server/src/Result.ts
+++ server/src/Result.ts
@@ -15,6 +15,6 @@ export const Err = <E, >(error: E) : Err<E> => ({ok: false, error: error})
 
 export type Result<T,E> = Ok<T> | Err<E>
 
-export const wrap = <Input, Output>(fn : (_:Input) => Result<Output, any>) => 
-  (res: Result<Input, any>) : Result<Output, any> => 
+export const wrap = <Input, Output, Err>(fn : (_:Input) => Result<Output, Err>) => 
+  (res: Result<Input, Err>) : Result<Output, Err> => 
     res.ok ? fn(res.data) : res
